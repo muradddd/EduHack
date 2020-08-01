37e53d7818fb5d752f57294b5cf56a8c520ee27c
İlahiye Musayeva <50544941+ilahiyye@users.noreply.github.com>
2020-08-01 21:43:01 +0400

    Update controllers.py

--- eduhack/core/controllers.py
+++ eduhack/core/controllers.py
@@ -5,3 +5,7 @@ core = Blueprint(__name__, 'core')
 @core.route('/', methods=['GET',])
 def home():
     return render_template('core/index.html')
+
+@core.route('/', methods=['GET','POST'])
+def home():
+    return render_template('core/teacher_main.html')
